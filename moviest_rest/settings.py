5493c4ec4cbb4a9d89f7ceb67f68316435c76a85
PunchTheSun <o.gold.me@gmail.com>
2023-03-22 17:20:50 +0200

    All Set for Lesson25

--- moviest_rest/settings.py
+++ moviest_rest/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'movies_rest_eve',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': '83199394',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
